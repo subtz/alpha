eb3ad68dfb5ebcb75e11e532ec1384fcdcf64d78
subtz <ezzyb498@gmail.com>
2025-05-09 13:20:15 +0300

    next step to production

--- calc/views.py
+++ calc/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render              # Import render to return an HT
 
 # Home view - renders the home.html template
 def home(request):
-    return render(request, 'home.html')          # Pass the request to home.html and render it
+    return render(request, 'calc/home.html')          # Pass the request to home.html and render it
 # calc/views.py
 from django.contrib.auth.forms import UserCreationForm  # Import the default user registration form
 from django.contrib.auth import login                    # Import login method to log in a user right after registration
@@ -18,7 +18,7 @@ def register(request):
             return redirect('dashboard')  # Redirect to the dashboard page after successful registration
     else:
         form = UserCreationForm()  # Create a blank form for GET request
-    return render(request, 'register.html', {'form': form})  # Render the register form in the template
+    return render(request, 'calc/register.html', {'form': form})  # Render the register form in the template
 # calc/views.py
 from django.contrib.auth.decorators import login_required  # Import the login_required decorator
 
