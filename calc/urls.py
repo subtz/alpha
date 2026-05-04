db11c0c1f5c5a63436b0328160ac1e122b4794b5
Ezzy <ezzyb498@gmail.com>
2026-05-05 00:36:08 +0300

    Fix queue list rendering and add pause/resume toggle

--- calc/urls.py
+++ calc/urls.py
@@ -37,4 +37,5 @@ urlpatterns = [
     path('staff/dashboard/', views.queue_control_dashboard, name='queue_control_dashboard'),
     path('staff/serve/', views.serve_current, name='serve_current'),
     path('staff/skip/', views.skip_current, name='skip_current'),
+    path('staff/toggle/<int:queue_id>/', views.toggle_queue_pause, name='toggle_queue_pause'),
 ]
